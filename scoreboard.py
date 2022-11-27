31cfbab332ecc8ad86a46c5fb34daeb46072bfac
nreh1r <87240487+nreh1r@users.noreply.github.com>
2022-11-27 16:27:40 -0700

    Added Highscore Storage

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,7 @@ FONT = ("Courier", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.high_score = self.get_high_score()
         self.score = 0
         self.display()
 
@@ -19,14 +20,26 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        text = f"Score: {self.score}"
+        self.clear()
+        text = f"Score: {self.score} High Score: {self.high_score}"
         self.write(text, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as file:
+                file.write(str(self.score))
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
+
+    def get_high_score(self):
+        with open("data.txt") as file:
+            return int(file.read())
